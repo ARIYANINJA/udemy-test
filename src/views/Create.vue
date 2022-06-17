aaa47f21f73e2a7cf0e60f8a6351b72938fc6b57
Ariyaninj <ariyaninj@gmail.com>
2022-06-18 01:40:50 +0800

    someChanges

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,80 +1,89 @@
 <template>
   <div class="create">
-      <form @submit.prevent = "handleSubmit">
-          <label>Title: </label>
-          <input v-model="title" type="text" required>
-          <label>Content: </label>
-          <textarea v-model="body" required></textarea>
-          <label>Tags: (hit enter to add a tag)</label>
-          <input v-model="tag" type="text" @keydown.enter.prevent = handleKeydown>
-          <div v-for="tag in tags " :key="tag" class="pill">
-              #{{tag}}
-          </div>
-          <button @click="handleSubmit()">Add Post</button>
-      </form>
+    <form @submit.prevent="handleSubmit">
+      <label>Title:</label>
+      <input v-model="title" type="text" required>
+      <label>Content:</label>
+      <textarea v-model="body" required></textarea>
+      <label>Tags (hit enter to add a tag):</label>
+      <input 
+        @keydown.enter.prevent="handleKeydown" 
+        v-model="tag" 
+        type="text"
+      >
+      <div v-for="tag in tags" :key="tag" class="pill">
+        #{{ tag }}
+      </div>
+      <button>Add Post</button>
+    </form>
   </div>
 </template>
 
 <script>
-import {ref} from 'vue'
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
 export default {
-    setup(){
-        const title = ref('')
-        const body = ref('')
-        const tag = ref('')
-        const tags = ref([])
-
-        const handleKeydown = () =>{
-            if(!tags.value.includes(tag.value)){
-                tag.value = tag.value.replace(/\s/,'') // remove all whitespaces
-                tags.value.push(tag.value)
-            }
-            tag.value = ''
-        }
-        const handleSubmit = async () =>{
-            const post = {
-                title: title.value,
-                body: body.value,
-                tags: tags.value,
-            }
-
-            await fetch('http://localhost:3000/posts',{
-                method: 'POST',
-                headers:{'Content-Type': 'application/json'},
-                body: JSON.stringify(post)
-            })
-        }
-        return {title, body, tag, handleKeydown, tags, handleSubmit}
+  setup() {
+    const title = ref('')
+    const body = ref('')
+    const tags = ref([])
+    const tag = ref('')
+    const router = useRouter()
+    //console.log(router)
+    //router.go(1)
+    //router.go(-1)
+    const handleKeydown = () => {
+      if (!tags.value.includes(tag.value)) {
+        tag.value = tag.value.replace(/\s/g,'') // remove all whitespace
+        tags.value.push(tag.value)
+      }
+      tag.value = ''
     }
+    const handleSubmit = async () => {
+      const post = {
+        id: Math.floor(Math.random() * 10000),
+        title: title.value,
+        body: body.value,
+        tags: tags.value
+      }
+      await fetch('http://localhost:3000/posts', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify(post)
+      })
+      router.push({ name: 'Home' })
+    }
+    return { body, title, tags, tag, handleKeydown, handleSubmit }
+  },
 }
 </script>
 
-<style scoped>
-form{
-    max-width: 480px;    
+<style>
+  form {
+    max-width: 480px;
     margin: 0 auto;
-    text-align: center;
-}
-input , textarea{
+    text-align: left;
+  }
+  input, textarea {
     display: block;
     margin: 10px 0;
     width: 100%;
     box-sizing: border-box;
     padding: 10px;
-    border: solid 1px #eee;
-}
-textarea{
+    border: 1px solid #eee;
+  }
+  textarea {
     height: 160px;
-}
-label{
+  }
+  label {
     display: inline-block;
     margin-top: 30px;
     position: relative;
     font-size: 20px;
     color: white;
     margin-bottom: 10px;
-}
-label::before{
+  }
+  label::before {
     content: "";
     display: block;
     width: 100%;
@@ -84,24 +93,24 @@ label::before{
     z-index: -1;
     padding-right: 40px;
     left: -30px;
-    transform: rotateZ(-1.5deg)
-}
-button{
+    transform: rotateZ(-1.5deg);
+  }
+  button {
     display: block;
     margin-top: 30px;
     background: #ff8800;
     color: white;
     border: none;
     padding: 8px 16px;
-    font-size: 18px;
-}
-.pill{
+    font-size: 18px
+  }
+  .pill {
     display: inline-block;
     margin: 10px 10px 0 0;
     color: #444;
-    background: #eee;
+    background: #ddd;
     padding: 8px;
     border-radius: 20px;
     font-size: 14px;
-}
+  }
 </style>
